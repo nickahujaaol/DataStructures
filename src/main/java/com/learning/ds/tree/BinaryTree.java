a6a3dd6975d55a92121296bd8362255e9ed2d020
nickahujaaol <nick_ahuja@aol.com>
2020-05-22 05:14:29 -0700

    Initial commit

---
+++ src/main/java/com/learning/ds/tree/BinaryTree.java
@@ -0,0 +1,45 @@
+package com.learning.ds.tree;
+
+public class BinaryTree {
+	public TreeNode root;
+
+	public BinaryTree() {
+		root = null;
+	}
+
+	/*
+											8
+								  /                 \
+								/                     \
+							4                           11
+						  /    \                     /       \
+					   2         6               9               13
+					  /  \     /   \              \            /    \
+					1     3   5      7               10      12      14
+	 */
+	public static BinaryTree create() {
+		BinaryTree tree = new BinaryTree();
+		TreeNode root = new TreeNode(8);
+		tree.root = root;
+
+
+		root.left = new TreeNode(4);
+		root.left.left = new TreeNode(2);
+		root.left.left.left = new TreeNode(1);
+		root.left.left.right = new TreeNode(3);
+
+		root.left.right = new TreeNode(6);
+		root.left.right.left = new TreeNode(5);
+		root.left.right.right = new TreeNode(7);
+
+		root.right = new TreeNode(11);
+		root.right.left = new TreeNode(9);
+		root.right.left.right = new TreeNode(10);
+
+		root.right.right = new TreeNode(13);
+		root.right.right.left = new TreeNode(12);
+		root.right.right.right = new TreeNode(14);
+
+		return tree;
+	}
+}
---
+++ src/main/java/com/learning/ds/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package com.learning.ds.tree;
+
+public class TreeNode {
+	public int value;
+	public TreeNode left;
+	public TreeNode right;
+
+	TreeNode(int value) {
+		this.value = value;
+		left = right = null;
+	}
+}
